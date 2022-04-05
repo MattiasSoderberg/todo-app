a4d15e5dde628d2649f0d2e5e42aeefa1446f3e9
Mattias Söderberg <mattes.email@gmail.com>
2022-04-05 14:57:43 +0200

    Add token verification

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const cors = require("cors")
 require("dotenv").config()
+
+const userRouter = require("./routes/user")
+const todoRouter = require("./routes/todo")
+
+const { verifyToken } = require("./middlewares/auth")
 
 const app = express()
 
@@ -9,6 +15,13 @@ const MONGODB_URI = process.env.MONGODB_URI_LOCAL
 
 mongoose.connect(MONGODB_URI)
     .then(() => {
+        app.use(express.json())
+        app.use(cors())
+        app.use(verifyToken)
+
+        app.use("/api/users", userRouter)
+        app.use("/api/todos", todoRouter)
+
         app.listen(PORT, () => {
             console.log(`Listening on port ${PORT}`)
         })
