2f60837c7145789a426f5f2efca43b08ab352790
Mattias Söderberg <mattes.email@gmail.com>
2022-04-05 14:59:30 +0200

    Minor fixes

--- server/models/User.js
+++ server/models/User.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose")
 const bcrypt = require("bcryptjs")
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true, select: false }
 })
 
 userSchema.pre("save", async function (next) {
     if (this.modifiedPaths().includes("password")) {
-        const hash = bcrypt.hash(this.password, 10)
+        const hash = await bcrypt.hash(this.password, 10)
         this.password = hash
     }
     next()
@@ -21,7 +21,7 @@ const createUser = async (userdata) => {
 }
 
 const login = async (username, password) => {
-    const user = await User.findOne({ username })
+    const user = await User.findOne({ username }).select("+password")
     if (user) {
         const passwordIsValid = await bcrypt.compare(password, user.password)
         if (passwordIsValid) return user
@@ -29,4 +29,8 @@ const login = async (username, password) => {
     return null
 }
 
-module.exports = { createUser, login }
+const getUser = async (username) => {
+    return await User.findOne({ username })
+}
+
+module.exports = { createUser, login, getUser }
