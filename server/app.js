6b175c055799b53089e19eb216fdbac0ddb7ef19
Mattias Söderberg <mattes.email@gmail.com>
2022-04-05 09:15:53 +0200

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+const app = express()
+
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI_LOCAL
+
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Listening on port ${PORT}`)
+        })
+    })
+    .catch(err => {
+        console.log("Mongo error:", err)
+    })
