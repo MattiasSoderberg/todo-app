171867b6488341cec5b7eb082892876f9bf08db7
Mattias Söderberg <mattes.email@gmail.com>
2022-04-19 14:20:12 +0200

    Add navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,12 @@ import { Heading, Flex } from "@chakra-ui/react"
 import LoginPage from "./pages/LoginPage";
 import UserCreatePage from "./pages/UserCreatePage";
 import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Flex w="100vw" h="100vh" justify="center" py="3rem">
+    <Flex w="100vw" h="100vh" direction="column" align="center">
+      <Navbar />
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/create-user" element={<UserCreatePage />} />
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import {
+    Flex, Heading, IconButton, Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    Input,
+    InputGroup,
+    InputLeftElement
+} from '@chakra-ui/react'
+import { HamburgerIcon, SearchIcon } from "@chakra-ui/icons"
+
+export default function Navbar() {
+    const handleLogout = () => {
+        console.log("logout")
+    }
+
+    return (
+        <Flex
+            w="100%"
+            h="100px"
+            bg="pink.100"
+            align="center"
+            mb="1rem"
+            justify="space-between"
+            px="2rem"
+        >
+                <Heading as="h1" size="2xl">Todo-app</Heading>
+                <InputGroup w="16rem" borderColor="gray.500" >
+                    <InputLeftElement
+                        pointerEvents='none'
+                        children={<SearchIcon color='gray.500' />}
+                    />
+                    <Input placeholder="Search" focusBorderColor="gray.500" />
+                </InputGroup>
+
+                <Menu direction="rtl">
+                    <MenuButton as={IconButton}
+                        aria-label='Options'
+                        icon={<HamburgerIcon />}
+                        variant='outline'
+                        borderColor="gray.500"
+                        bg="pink.100"
+                        size="lg" />
+                    <MenuList>
+                        <MenuItem>Download</MenuItem>
+                        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                    </MenuList>
+                </Menu>
+        </Flex>
+    )
+}
